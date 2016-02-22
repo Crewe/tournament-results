c2d9574c64526191a8ca6a76e22dc350a2c46ca7
Ryan Edwards-Crewe <ryan@ryancrewe.com>
2016-02-22 00:02:55 -0500

    Removed unrelated projects

--- README.md
+++
@@ -1,6 +0,0 @@
-### Project instructions are located in the sub-folders.
-
-* Project 1 -> movie-website
-* Project 2 -> tournament-results
-* Project 3 -> item-catalog
-* Project 4 -> conference-central
--- conference-central/appengine_config.py
+++
@@ -1,5 +0,0 @@
-def webapp_add_wsgi_middleware(app):
-    from google.appengine.ext.appstats import recording
-    app = recording.appstats_wsgi_middleware(app)
-    appstats_CALC_RPC_COSTS = True
-    return app
